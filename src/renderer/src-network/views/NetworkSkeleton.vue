79c853e743988c0376599b19781f06c5074ee24a
dkearns-lumination <dkearns@lumination.com.au>
2023-12-04 15:19:14 +1030

    Build skelton for network page one

--- src/renderer/src-network/views/NetworkSkeleton.vue
+++ src/renderer/src-network/views/NetworkSkeleton.vue
@@ -6,6 +6,7 @@ import NetworkSection from "./NetworkSection.vue";
 import {Swiper, SwiperSlide, useSwiper} from "swiper/vue";
 import {ref} from "vue";
 import 'swiper/css';
+import NetworkDeviceSelection from "./NetworkDeviceSelection.vue";
 
 function up() {
   swiperRef.value.$el.swiper.slideNext(500)
@@ -32,28 +33,22 @@ function setSwiper(s) {
 <template>
   <div class="w-full h-full flex flex-col justify-between">
     <div class="flex flex-row">
-      <NetworkHeader class="h-20"/>
-      <button @click="down">Back</button>
-      <button @click="up">Next</button>
+      <NetworkHeader/>
     </div>
     <div class="flex flex-row w-full h-full">
-      <NetworkSidebar class="w-32" :number-of-sections="4" :current-section="swiper?.activeIndex" @select="navigateTo"/>
-
-      <Swiper ref="swiperRef" @swiper="setSwiper" direction="vertical" :pagination="{ clickable: true }" :slides-per-view="1" class="w-full h-full">
+      <NetworkSidebar class="w-32 pt-10" :number-of-sections="4" :current-section="swiper?.activeIndex" @select="navigateTo"/>
+      <Swiper ref="swiperRef" @swiper="setSwiper" direction="vertical" :pagination="{ clickable: true }" :slides-per-view="1" class="w-full h-auto pt-10">
         <SwiperSlide>
-          <NetworkSection class="bg-red-100 w-full h-full">
-            <template v-slot:step>Step 1</template>
-            <template v-slot:heading>Heading for page 1</template>
-          </NetworkSection>
+          <NetworkDeviceSelection/>
         </SwiperSlide>
         <SwiperSlide>
-          <NetworkSection class="bg-green-100 w-full h-full">
+          <NetworkSection class="w-full">
             <template v-slot:step>Step 2</template>
             <template v-slot:heading>Short</template>
           </NetworkSection>
         </SwiperSlide>
         <SwiperSlide>
-          <NetworkSection class="bg-blue-100 w-full h-full">
+          <NetworkSection class="w-full">
             <template v-slot:step>Step 3</template>
             <template v-slot:body><p>Oh my god<br/>
               so much text<br/>
@@ -63,7 +58,7 @@ function setSwiper(s) {
           </NetworkSection>
         </SwiperSlide>
         <SwiperSlide>
-          <NetworkSection class="bg-yellow-100 w-full h-full">
+          <NetworkSection class="w-full">
             <template v-slot:step>Step 4</template>
             <template v-slot:heading>Short</template>
           </NetworkSection>
