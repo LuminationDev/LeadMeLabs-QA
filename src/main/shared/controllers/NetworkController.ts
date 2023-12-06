ab389137364f7c4360f8323bdcec5ec179c5f6e1
dkearns-lumination <dkearns@lumination.com.au>
2023-12-06 11:50:03 +1030

    Windows check before running routing commands

--- src/main/shared/controllers/NetworkController.ts
+++ src/main/shared/controllers/NetworkController.ts
@@ -148,6 +148,13 @@ export default class NetworkController {
     }
 
     async buildPortCheck() {
+        if (!process.platform.includes("win")) {
+            this.mainWindow.webContents.send('backend_message', {
+                channelType: "build_port_check",
+                result: false
+            });
+            return;
+        }
         await this.runRouteCommand(true).catch((error) => {
             this.mainWindow.webContents.send('backend_message', {
                 channelType: "build_port_check",
@@ -161,6 +168,13 @@ export default class NetworkController {
     }
 
     async teardownPortCheck() {
+        if (!process.platform.includes("win")) {
+            this.mainWindow.webContents.send('backend_message', {
+                channelType: "teardown_port_check",
+                result: false
+            });
+            return;
+        }
         await this.runRouteCommand(false).catch((error) => {
             this.mainWindow.webContents.send('backend_message', {
                 channelType: "teardown_port_check",
