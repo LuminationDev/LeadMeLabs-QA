53d3c30e0debd67caf5e60d8a891706333b81491
dkearns-lumination <dkearns@lumination.com.au>
2023-12-11 10:11:54 +1030

    Implement CI/CD for network

--- config/config-network/package.main.js
+++ config/config-network/package.main.js
@@ -2,5 +2,5 @@ module.exports = {
     main: "build/main/main-network/main.js",
     name: "leadme-network",
     version: "1.0.0",
-    description: "The LeadMe Labs QA Network"
+    description: "The LeadMe Labs Network Tool"
 };
