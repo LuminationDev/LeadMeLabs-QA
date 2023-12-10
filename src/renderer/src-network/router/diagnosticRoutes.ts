859bc5bcc0d7d9259caf29b7b9e4e82da67c5aa1
dkearns-lumination <dkearns@lumination.com.au>
2023-12-11 08:37:23 +1030

    Standalone network layout

--- src/renderer/src-network/router/diagnosticRoutes.ts
+++ src/renderer/src-network/router/diagnosticRoutes.ts
@@ -2,6 +2,15 @@ import NetworkSkeleton from "../views/NetworkSkeleton.vue";
 
 export const diagnosticRoutes = [
     {
+        path: '/',
+        name: 'network-diagnostic-main',
+        component: NetworkSkeleton,
+        meta: {
+            prev: '/',
+            progress: 0
+        }
+    },
+    {
         path: '/network-diagnostic',
         name: 'network-diagnostic',
         component: NetworkSkeleton,
