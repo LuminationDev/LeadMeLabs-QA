c95538831181f213de5d5d7ce32ba96a57100550
dkearns-lumination <dkearns@lumination.com.au>
2023-12-12 13:35:48 +1030

    Bump version for auto download test

--- config/config-network/package.main.js
+++ config/config-network/package.main.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: "build/main/main-network/main.js",
     name: "leadme-network",
-    version: "1.0.4",
+    version: "1.0.5",
     description: "The LeadMe Labs Network Tool"
 };
