ce998529f038194c8fe242d87add3d379952b909
dkearns-lumination <dkearns@lumination.com.au>
2023-12-12 11:25:30 +1030

    Test auto update

--- config/config-network/package.main.js
+++ config/config-network/package.main.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: "build/main/main-network/main.js",
     name: "leadme-network",
-    version: "1.0.2",
+    version: "1.0.3",
     description: "The LeadMe Labs Network Tool"
 };
