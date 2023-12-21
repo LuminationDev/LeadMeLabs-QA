ba6dcd7b46f715056b356e044971563d54e3e68b
dkearns-lumination <dkearns@lumination.com.au>
2023-12-21 13:46:34 +1030

    Can now add 'details' to the QA check, and add the windows version

--- src/renderer/src-qa/components/Report/Submission/PDFDeviceStatus.vue
+++ src/renderer/src-qa/components/Report/Submission/PDFDeviceStatus.vue
@@ -23,6 +23,7 @@ defineProps({
       <div v-else-if="checkingStatus === 'timeout' || (passedStatus === 'failed' && checkingStatus !== 'checking')" class="bg-yellow-500 h-3 w-3 rounded-xl"/>
       <div v-else-if="passedStatus === 'failed' && checkingStatus !== 'checking'" class="bg-red-500 h-3 w-3 rounded-xl"/>
       <div v-else-if="passedStatus === 'passed' && checkingStatus !== 'checking'" class="bg-green-500 h-3 w-3 rounded-xl"/>
+      <div v-else-if="passedStatus === 'detail' && checkingStatus !== 'checking'" class="bg-blue-500 h-3 w-3 rounded-xl"/>
       <div v-else-if="passedStatus === 'warning' && checkingStatus !== 'checking'" class="bg-amber-500 h-3 w-3 rounded-xl"/>
       <div v-else-if="passedStatus === 'not_applicable' && checkingStatus !== 'checking'">N/A</div>
       <div v-else class="border border-gray-800 h-3 w-3 rounded-xl"/>
