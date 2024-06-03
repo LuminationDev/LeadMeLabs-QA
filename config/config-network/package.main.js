e7f4e0d4224601d57d6ef9d19677d172f3e84da5
dkearns-lumination <99624400+dkearns-lumination@users.noreply.github.com>
2024-06-03 14:10:57 +0930

    Update package.main.js

--- config/config-network/package.main.js
+++ config/config-network/package.main.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: "build/main/main-network/main.js",
     name: "leadme-network",
-    version: "1.1.2",
+    version: "1.1.3",
     description: "The LeadMe Labs Network Tool"
 };
