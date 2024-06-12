2599fd27361404b0a9b6a92cc9f2b809e6fae81d
dkearns-lumination <99624400+dkearns-lumination@users.noreply.github.com>
2024-06-12 10:00:50 +0930

    Update package.main.js

--- config/config-full/package.main.js
+++ config/config-full/package.main.js
@@ -1,6 +1,6 @@
 module.exports = {
     main: "build/main/main-full/main.js",
     name: "leadme-tools-qa",
-    version: "1.0.20",
+    version: "1.0.21",
     description: "The LeadMe Labs QA tools"
 };
