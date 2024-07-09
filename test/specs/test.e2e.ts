e141ad5a3befcb123b2c965a88575fb3206853b4
Erick Hartawan <ehartawan@lumination.com.au>
2024-07-09 11:20:00 +0930

    initial setup test with wdio

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,9 @@
+import {browser} from '@wdio/globals'
+
+describe('Electron Testing', () => {
+    it('should print application title', async () => {
+        console.log('Hello', await browser.getTitle(), 'application!')
+        return true
+    })
+    return true
+})
